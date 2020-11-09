4e3653ac67f443bebd94128708fadbd815105656
y9k8i <28494556+y9k8i@users.noreply.github.com>
2020-11-09 15:35:25 +0900

    入力された検索文字列の種別を判断するように変更 進行状況表示を削除

--- main.py
+++ main.py
@@ -43,24 +43,36 @@ class ProgressScreen(Screen):
     query = ""
     update = False
     query_label = ObjectProperty(None)
-    progress = ObjectProperty(None)
 
     def on_pre_enter(self, *args):
         self.query_label.text = "検索文字列: " + self.query
 
         if self.debug:
             MyApp.logger.setLevel(logging.DEBUG)
-        self.progress.value = 1
-        network_address = self.dnsGetter.get_network_address(domain_name)
-        self.progress.value = 2
-        file_name = self.dnsGetter.get_DNS(network_address, "addr")
-        self.progress.value = 3
+        try:
+            self.dnsGetter = DNSGetter(
+                debug=self.debug, update=self.update, logger=MyApp.logger)
+            if self.query.rsplit('/')[0].replace('.', '').isdecimal():
+                file_name = self.dnsGetter.get_DNS(self.query, "addr")
+            elif self.query.rsplit('.', 1)[-1] == "html":
+                file_name = self.dnsGetter.get_DNS(self.query, "file")
+            elif self.query.replace('.', '').encode('utf-8').isalnum():
+                file_name = self.dnsGetter.get_DNS(
+                    self.dnsGetter.get_network_address(self.query), "addr")
+            else:
+                domain_name = self.dnsGetter.get_domain_name(self.query)
+                file_name = self.dnsGetter.get_DNS(
+                    self.dnsGetter.get_network_address(domain_name), "addr")
+                ResultScreen.domain_name = domain_name
+            ResultScreen.file_name = file_name
+            # numberreplacer.pyにおけるドメイン自動検出が不完全
+        except Exception as e:
+            MyApp.logger.error(e, exc_info=self.debug)
+            App.get_running_app().open_popup(str(e))
+        finally:
+            del self.dnsGetter
 
-        ResultScreen.file_name = file_name
         self.parent.current = "result"
-
-    def on_leave(self, *args):
-        del self.dnsGetter
 
 
 class ResultScreen(Screen):
